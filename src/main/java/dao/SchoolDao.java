f6c0cd1acaa5b806a0ab27d11f63725c55eb1434
Kira Loo <youhavebeenkiraed@gmail.com>
2017-08-25 16:20:52 -0700

    end for today need to go to a wedding.

--- src/main/java/dao/SchoolDao.java
+++ src/main/java/dao/SchoolDao.java
@@ -1,12 +1,17 @@
 package dao;
+import models.Course;
 import models.School;
+
+import java.util.List;
 
 
 public interface SchoolDao {
 
     ///..CREATE..///
     void add(School school);
+    void addCourseToType(School school, Course course);
     ///..READ..///
+    List<Course> getAllCoursesForTypes(int schoolId);
     ///..UPDATE..///
     ///..DELETE..///
 
--- src/main/java/dao/Sql2oSchoolDao.java
+++ src/main/java/dao/Sql2oSchoolDao.java
@@ -1,9 +1,13 @@
 package dao;
 
+import models.Course;
 import models.School;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sql2oSchoolDao implements SchoolDao {
     private final Sql2o sql2o;
@@ -25,4 +29,36 @@ public class Sql2oSchoolDao implements SchoolDao {
            System.out.println(ex);
        }
     }
-}
+    @Override
+    public void addCourseToType(School school, Course course) {
+        String query = "INSERT INTO school_students(schoolId, courseId) VALUES (:schoolId, :courseId)";
+        try(Connection con = sql2o.open()) {
+            con.createQuery(query)
+                    .addParameter("schoolId", school.getId())
+                    .addParameter("courseId", course.getId())
+                    .executeUpdate();
+        } catch (Sql2oException e){
+            System.out.println(e);
+        }
+    }
+    @Override
+    public List<Course> getAllCoursesForTypes(int schoolId) {
+        List<Course> courses = new ArrayList<>();
+        String query = "SELECT courseId FROM school_students WHERE schoolId = :schoolId";
+        try(Connection con = sql2o.open()) {
+            List<Integer> allCourseIds = con.createQuery(query)
+                    .addParameter("schoolId", schoolId)
+                    .executeAndFetch(Integer.class);
+            for (Integer courseId : allCourseIds) {
+                String query2 = "SELECT * from course WHERE id = courseId";
+                courses.add(
+                        con.createQuery(query2)
+                                .addParameter("courseId", courseId)
+                                .executeAndFetchFirst(Course.class));
+            }
+        }catch(Sql2oException e){
+            System.out.println(e);
+            }
+            return courses;
+        }
+    }
